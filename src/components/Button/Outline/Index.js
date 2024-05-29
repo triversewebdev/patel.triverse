0c73f20fcff5ba9213227b9599e507157a9d972c
Akashtriverse1252 <akash.triverse@gmail.com>
2024-05-29 15:45:27 +0530

    desgin change

--- src/components/Button/Outline/Index.js
+++ src/components/Button/Outline/Index.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import "../../Button/Button.css"
+import React from "react";
+import "../../Button/Button.css";
 
-const Index = ({ buttonText, _class, pageLink }) => {
-    return (
-      <a href={pageLink} className='outline-button'>
-      <div className={`btn-8 ${_class}`} data-aos="zoom-in" data-aos-offset="50" data-aos-duration="500" data-aos-once="true" data-aos-easing="ease-in-sine">       
-      {buttonText}
+const Index = ({ buttonText, _class, pageLink, OutlineDropdown }) => {
+  return (
+    <a href={pageLink} className="outline-button">
+      <div
+        className={`btn-8 ${_class}`}
+        data-aos="zoom-in"
+        data-aos-offset="50"
+        data-aos-duration="500"
+        data-aos-once="true"
+        data-aos-easing="ease-in-sine"
+      >
+        {buttonText}
       </div>
-      
+      {OutlineDropdown && (
+        <div className="dropdown-content">
+          <ul>
+            <li>Patient Login</li>
+            <li>Doctor Login</li>
+            <li>Employee Login</li>
+          </ul>
+        </div>
+      )}
     </a>
-    );
-  };
+  );
+};
 
-export default Index
+export default Index;
